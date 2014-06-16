8c82d8cab8cfdbeb28055bbd2c5fbaf947c8c7b4
Brian Butz <butzopower@gmail.com>
2014-06-15 23:11:25 -0700

    Bind local dates to firebase instead of using firebase directly

--- app.js
+++ app.js
@@ -3,8 +3,11 @@ angular.module("dateJar").controller("DateController", ["$scope", "$firebase", f
     var firebaseRef = new Firebase("https://fiery-fire-8662.firebaseio.com/date-jar");
     $scope.dates = $firebase(firebaseRef);
 
+    $scope.localDates = [];
+    $scope.dates.$bind($scope, "localDates");
+
     this.addADate = function(newDateText) {
-      $scope.dates.$add({title: newDateText, done: false});
+      $scope.localDates.push({title: newDateText, done: false});
       $scope.newDateText = "";
     };
 }]);
